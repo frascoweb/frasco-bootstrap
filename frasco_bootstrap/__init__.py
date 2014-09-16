283ab9266d740ec6da7e4d200a78eaf3a2ab269a
maximebf <maxime.bouroumeau@gmail.com>
2014-09-16 10:49:31 +0200

    first commit

---
+++ frasco_bootstrap/__init__.py
@@ -0,0 +1,50 @@
+from frasco import Feature, hook, current_context
+from frasco.templating import FileLoader
+from jinja2 import PackageLoader
+import os
+
+
+class BootstrapFeature(Feature):
+    name = "bootstrap"
+    requires = ["assets"]
+    defaults = {"auto_assets": True,
+                "with_jquery": True,
+                "with_fontawesome": False,
+                "fluid_layout": False}
+
+    def init_app(self, app):
+        path = os.path.dirname(__file__)
+        app.jinja_env.macros.register_package(__name__, prefix="bootstrap")
+        app.jinja_env.loader.feature_loaders.append(PackageLoader(__name__))
+        app.jinja_env.loader.set_layout_alias("bootstrap_layout.html")
+
+        app.assets.register({
+            "bootstrap-cdn": [
+                "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css",],
+            "bootstrap-theme-cdn": [
+                "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap-theme.min.css"],
+            "bootstrap-js-cdn": [
+                "https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/js/bootstrap.min.js"],
+            "bootstrap-all-cdn": [
+                "@bootstrap-cdn",
+                "@bootstrap-theme-cdn",
+                "@bootstrap-js-cdn"],
+            "jquery-cdn": [
+                "https://code.jquery.com/jquery-2.1.1.min.js"],
+            "jquery-bootstrap-all-cdn": [
+                "@jquery-cdn",
+                "@bootstrap-all-cdn"],
+            "font-awesome-cdn": [
+                "http://maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css"]})
+
+        if self.options["auto_assets"]:
+            if self.options["with_fontawesome"]:
+                app.assets.defaults.insert(0, "@font-awesome-cdn")
+            if self.options["with_jquery"]:
+                app.assets.defaults.insert(0, "@jquery-bootstrap-all-cdn")
+            else:
+                app.assets.defaults.insert(0, "@bootstrap-all-cdn")
+
+    @hook()
+    def before_request(self):
+        current_context["bs_layout_fluid"] = self.options["fluid_layout"]
---
+++ setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup, find_packages
+
+
+def desc():
+    with open("README.md") as f:
+        return f.read()
+
+
+setup(
+    name='frasco-bootstrap',
+    version='0.1',
+    url='http://github.com/frascoweb/frasco-bootstrap',
+    license='MIT',
+    author='Maxime Bouroumeau-Fuseau',
+    author_email='maxime.bouroumeau@gmail.com',
+    description="Bootstrap (frontend framework) integration for Frasco",
+    long_description=desc(),
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    platforms='any',
+    install_requires=[
+        'frasco',
+        'frasco-assets'
+    ]
+)
