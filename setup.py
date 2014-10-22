54ef85e1138d371744a24a8348d967cf686b37b0
maximebf <maxime.bouroumeau@gmail.com>
2014-10-23 00:33:21 +0200

    updated setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 from setuptools import setup, find_packages
 
 
-def desc():
-    with open("README.md") as f:
-        return f.read()
-
-def reqs():
-    with open('requirements.txt') as f:
-        return f.read().splitlines()
-
 setup(
     name='frasco-bootstrap',
     version='0.1',
@@ -17,13 +9,17 @@ setup(
     author='Maxime Bouroumeau-Fuseau',
     author_email='maxime.bouroumeau@gmail.com',
     description="Bootstrap (frontend framework) integration for Frasco",
-    long_description=desc(),
     packages=find_packages(),
-    include_package_data=True,
+    package_data={
+        'frasco_bootstrap': [
+            'macros/*.html',
+            'templates/*.html',
+            'templates/users/*.html']
+    },
     zip_safe=False,
     platforms='any',
     install_requires=[
-        'frasco',
-        'frasco-assets'
+        # 'frasco',
+        # 'frasco-assets'
     ]
 )
