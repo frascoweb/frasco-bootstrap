489da12df7c7d22f09d6da9f624ca76c56a720b7
maximebf <maxime.bouroumeau@gmail.com>
2015-07-27 16:22:20 +0200

    updated version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='frasco-bootstrap',
-    version='0.1.1',
+    version='0.1.2',
     url='http://github.com/frascoweb/frasco-bootstrap',
     license='MIT',
     author='Maxime Bouroumeau-Fuseau',
