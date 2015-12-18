de034f7d717813300963ae8894f6955e833e097c
maximebf <maxime.bouroumeau@gmail.com>
2015-12-18 09:50:45 +0100

    supper for custom attributes on bs_menu_item macro

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='frasco-bootstrap',
-    version='0.1.2',
+    version='0.1.3',
     url='http://github.com/frascoweb/frasco-bootstrap',
     license='MIT',
     author='Maxime Bouroumeau-Fuseau',
